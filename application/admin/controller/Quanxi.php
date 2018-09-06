86e1c2ea3d0427f0eae81ff9db699c6ad55b16b3
zhh <1239445499@qq.com>
2018-09-06 21:12:22 +0800

    权限1.2.3

--- application/admin/controller/Quanxi.php
+++ application/admin/controller/Quanxi.php
@@ -49,6 +49,33 @@ class Quanxi extends Controller
 
 		return view('privilege_add');
 	}
+	public function privilege_edit()
+	{
+		$id = input('id');
+		// echo $id;exit;
+		$data =Db('admin')->where('admin_id='.$id)->select();
+		$this->assign('list', $data);		
+		if(request()->isPost()){
+//			dump(input('post.')); //打印接收到的参数
+ 
+			$data = [		//接受传递的参数
+				'admin_name' => input('user_name'),
+				'admin_email' => input('email'),
+				'admin_pwd' => md5(input('password')),
+				// 'date_time'=> date("Y-m-d H:i:s",time()),
+			];
+			
+		/*	Db('表名') 数据库助手函数*/
+			if(Db('admin')->where('admin_id='.$id)->update($data)){		//添加数据
+				return $this->success('修改管理员成功',"admin/Quanxi/privilege");	//成功后跳转  lst 界面
+			}else{
+				return $this->error('修改管理员失败');
+			}
+			return;
+		
+		}
+		return $this->fetch('privilege_edit');
+	}
 	public function role()
 	{
 		return view('role');
@@ -60,5 +87,9 @@ class Quanxi extends Controller
 	public function suppliers()
 	{
 		return view('suppliers');
+	}
+	public function suppliers_edit()
+	{
+		return view('suppliers_edit');
 	}
 }
