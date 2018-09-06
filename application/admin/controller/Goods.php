3a7c80c0a9200c44a8e3eba0ae5be10ec37cdf79
your name <your email>
2018-09-06 21:16:33 +0800

    及点击该未完成

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -4,7 +4,9 @@ use think\Controller;
 use think\Model;
 use think\Paginator;
 class Goods extends Controller
-{
+{   
+
+    //model调用
     public $goods;
     public function _initialize(){
         $this->goods = model('Goods');
@@ -14,6 +16,33 @@ class Goods extends Controller
     {
         $res = $this->goods->goods_Show();
         return view('goods_list',['res'=>$res]);
+    }
+    //商品列表及点击该
+    public function goods_change_put()
+    {
+        $goods_id = input('get.goods_id');
+        $enabled = input('get.enabled');
+
+        if($enabled==1){
+
+            $res = Db("goods")->where("goods_id",$goods_id)->update(["enabled"=>'0']);
+        }else{
+
+            $res = Db("goods")->where("goods_id",$goods_id)->update(["enabled"=>'1']);
+        }
+
+        if($res){
+
+            $arr['status'] = 0;
+            $arr['data'] = '';
+            $arr['msg'] = '成功';
+        }else{
+
+            $arr['status'] = 1;
+            $arr['data'] = '';
+            $arr['msg'] = '失败';
+        }
+        echo json_encode($arr);
     }
 
     public function goods_add()
