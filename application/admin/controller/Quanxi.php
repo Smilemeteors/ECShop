7b06c1bab4caef86da450722bfbdbc88dc6c99e0
zhh <1239445499@qq.com>
2018-09-06 18:20:37 +0800

    权限

--- application/admin/controller/Quanxi.php
+++ application/admin/controller/Quanxi.php
@@ -1,7 +1,8 @@
 <?php
 namespace app\admin\controller;
 
-class Quanxi
+use think\Controller;
+class Quanxi extends Controller
 {
 	public function admin_logs()
 	{
@@ -17,10 +18,35 @@ class Quanxi
 	}
 	public function privilege()
 	{
-		return view('privilege');
+		
+		$data =Db('admin')->select();
+		// var_dump($data);die;
+		$this->assign('list', $data);
+		return $this->fetch('privilege');
 	}
 	public function privilege_add()
 	{
+		
+		if(request()->isPost()){
+//			dump(input('post.')); //打印接收到的参数
+ 
+			$data = [		//接受传递的参数
+				'admin_name' => input('user_name'),
+				'admin_email' => input('email'),
+				'admin_pwd' => md5(input('password')),
+				'date_time'=> date("Y-m-d H:i:s",time()),
+			];
+			
+		/*	Db('表名') 数据库助手函数*/
+			if(Db('admin') -> insert($data)){		//添加数据
+				return $this->success('添加成功',"admin/Quanxi/privilege");	//成功后跳转  lst 界面
+			}else{
+				return $this->error('添加管理员失败');
+			}
+			return;
+		
+		}
+
 		return view('privilege_add');
 	}
 	public function role()
