0d57579603b0e9333d4b6846500d59d08adbfcb9
380536583@qq.com <380536583@qq.com>
2018-09-06 09:11:43 +0800

    logo

---
+++ application/admin/controller/Logo.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\admin\controller;
+
+class Logo
+{
+    public function logo()
+    {
+        
+        return view('logo');
+    }
+
+    public function position()
+    {
+    	return view('position');
+    }
+    
+}
