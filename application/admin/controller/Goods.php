d041f26977d8f677b8e34f845e7ac7a2c56ae36a
your name <your email>
2018-09-07 15:37:16 +0800

    最新goods

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -4,6 +4,7 @@ use think\Controller;
 use think\Db;
 use think\Model;
 use think\Paginator;
+use think\Request;
 class Goods extends Controller
 {   
 
@@ -22,23 +23,18 @@ class Goods extends Controller
     public function goods_change_put()
     {
         $goods_id = input('get.goods_id');
-        $enabled = input('get.enabled');
-
-        if($enabled==1){
-
-            $res = Db("goods")->where("goods_id",$goods_id)->update(["enabled"=>'0']);
+        $status = input('get.status');
+        if($status==1){
+            $res = Db("goods")->where("goods_id",$goods_id)->update(["status"=>'0']);
         }else{
-
-            $res = Db("goods")->where("goods_id",$goods_id)->update(["enabled"=>'1']);
+            $res = Db("goods")->where("goods_id",$goods_id)->update(["status"=>'1']);
         }
-
+        echo $res;die;
         if($res){
-
             $arr['status'] = 0;
             $arr['data'] = '';
             $arr['msg'] = '成功';
         }else{
-
             $arr['status'] = 1;
             $arr['data'] = '';
             $arr['msg'] = '失败';
@@ -49,6 +45,83 @@ class Goods extends Controller
     public function goods_add()
     {
         return view('goods_add');
+    }
+    
+    //品牌部分
+    public function brand_list(){
+        $res = $this->goods->brand_show();
+        return view('brand_list',['res'=>$res]);
+    }
+    //品牌添加
+    public function brand_add(){
+        return view('brand_add');
+    }
+
+    public function brand_add_do(){
+        echo'1';die;
+        $request = Request::instance()->post();
+        $request['brand_logo']=$this->brand_upload();
+        $res=$this->goods->inserts($request);
+        if($res) {
+            echo "<script>alert('上传成功');location.href='brand_list'</script>";
+        }else{
+            echo "<script>alert('上传失败');location.href='brand_add'</script>";
+        }
+    }
+    //文件上传
+    public function brand_upload(){
+       // 获取表单上传文件 例如上传了001.jpg
+       $file = request()->file('brand_logo');
+       // 移动到框架应用根目录/public/uploads/ 目录下
+       $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+       if($info){
+        // 成功上传后 获取上传信息
+        // 输出 jpg
+        // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+         return $info->getSaveName();
+        // 输出 42a79759f284b767dfcb2a0197904287.jpg
+        }else{
+        // 上传失败获取错误信息
+           return $file->getError();
+        }
+    }
+
+         //类型
+    public function goods_type_manage()
+    {
+        $arr = Db::table('goods_type')->select();
+        return view('goods_type_manage',['arr'=>$arr]);
+    }
+
+    public function goods_type_add()
+    {
+        return view('goods_type_add');
+    }
+    public function goods_type_add_do(){
+        $data = Request::instance()->post();
+        $arr = Db::name('goods_type')->insert($data);
+        if($arr){
+            $this->success('添加成功','goods/goods_type_manage');
+        }else{
+            $this->error('添加失败','goods/goods_type_add');
+        }
+    }
+    public function goods_type_upd()
+    {
+        $id = Request::instance()->get('id');
+        $arr = Db::table('goods_type')->where('cat_id',$id)->select();
+        return view('goods_type_upd',['arr'=>$arr]);
+    }
+    public function goods_type_del()
+    {
+        $id = Request::instance()->get('id');
+//        print_r($id);die;
+        $arr = Db::table('goods_type')->where('cat_id',$id)->delete();
+        if($arr){
+            $this->success('删除成功','goods/goods_type_manage');
+        }else{
+            $this->error('删除失败','goods/goods_type_manage');
+        }
     }
     public function comment_manage_add()
     {
@@ -62,41 +135,8 @@ class Goods extends Controller
     {
         return view('goods_trash');
     }
-    
     public function category_list()
     {
         return view('category_list');
     }
-    public function brand_list(){
-        $res = $this->goods->brand_show();
-        return view('brand_list',['res'=>$res]);
-    }
-    public function brand_add(){
-        return view('brand_add');
-    }
-    public function brand_add_do(){
-        echo "brand_add_do";
-    }
-     //类型
-    public function goods_type_manage()
-    {
-        $arr = Db::table('goods_type')->select();
-        return view('goods_type_manage',['arr'=>$arr]);
-    }
-
-//    public function good_type_add()
-//    {
-//        $data = [
-//            ['foo' => 'bar', 'bar' => 'foo'],
-//            ['foo' => 'bar1', 'bar' => 'foo1'],
-//            ['foo' => 'bar2', 'bar' => 'foo2']
-//        ];
-//        $about = Db::name('goods_type')->insertAll($data);
-//    }
-//    public function good_type_upd()
-//    {
-//        $about = Db::table('goods_type')->where('id', 1)->update(['name' => 'thinkphp']);
-//    }
-//    public function good_type_del(){}
-
 }
