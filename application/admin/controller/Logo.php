f31857bdb198d6c8d2f98692a64da4abab41a283
380536583@qq.com <380536583@qq.com>
2018-09-07 19:31:15 +0800

    logo

--- application/admin/controller/Logo.php
+++ application/admin/controller/Logo.php
@@ -9,9 +9,10 @@ class Logo extends Controller
 {
     public function logo()
     {
-    	// 实例化model
-    	// $place = Db('place');
-    	// $data = $place->fetchAll();  
+    	$place = Db('place');
+    	$data = $place->select(); 
+        // print_r($data);die; 
+        $this->assign('arr',$data);
         return view('logo');
     }
 
@@ -21,10 +22,9 @@ class Logo extends Controller
     		//获取需要入库的数据
     		$data = Request::instance()->post();
     		$data['ad_img'] = $this->upload();
-    		 // print_r($data['ad_img']);die;
-    		// 实例化model
+    		// print_r($data['ad_img']);die;
     		$res = Db('place')->insert($data);  
-    		// print_r($data);die;	
+    		return view('logo');	
     	} else {
     		return view('addLogo');
     	}   	
@@ -47,14 +47,69 @@ class Logo extends Controller
     	}
 	}
 
+    public function upLogo()
+    {
+        
+    }
+
+    public function delLogo()
+    {
+        $id = $_GET['id'];
+        
+    }
+
     public function position()
     {
+        $position = Db('position');
+        $data = $position->select();
+        $this->assign('arr',$data);
     	return view('position');
     }
 
     public function addPosition()
     {
-    	return view('addPosition');
+        if ($_POST) {
+            $arr = Request::instance()->post();
+            // print_r($arr);die;
+            $arr['position_style'] = $this->load();
+            // print_r($arr['position_style']);die;
+            $res = Db('position')->insert($arr);
+            // print_r($res);die;
+            return view('position');
+        } else {
+            return view('addPosition');
+        }	
+    }
+
+    public function load(){
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('position_style');      
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public'.'/uploads');
+           // var_dump($info);die;
+            if($info){
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                return $info->getFilename(); 
+            }else{
+                // 上传失败获取错误信息
+                return $file->getError();
+            }
+        }
+    }
+
+    public function upPosition()
+    {
+
+    }
+
+    public function delPosition()
+    {
+        $id = $_GET['id'];
     }
     
+    public function detail()
+    {
+
+    }
 }
