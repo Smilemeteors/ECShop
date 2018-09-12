eb6ab21ea3ed0d277c5815a02c0a1f4445432cef
993811779 <993811779@qq.com>
2018-09-12 16:16:13 +0800

    rcy

---
+++ runtime/temp/432533f670da0fedc4b8d689559fd879.php
@@ -0,0 +1,3 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:80:"D:\PHPTutorial\WWW\ECShop\public/../application/admin\view\member\integrate.html";i:1536285027;}*/ ?>
+﻿<br />
+<b>Parse error</b>:  syntax error, unexpected end of file in <b>D:\phpstudy\WWW\shixun\EC4\source\ecshop\admin\integrate.php</b> on line <b>1387</b><br />
